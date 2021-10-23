9e1c4dfa1467bf7cde664f36b8841e990966ba53
Dartar <07kirgame@gmail.com>
2021-10-23 15:11:36 +0300

    back

--- index.js
+++ index.js
@@ -2,26 +2,12 @@ import express from "express";
 import path from "path";
 import fs from "fs";
 import mqtt from "mqtt";
+import { client } from "./serverdata.js";
 
 const app = express();
 const __dirname = path.resolve();
 const PORT = process.env.PORT ?? 3000;
-// const options = {
-//    broker = 'mqtt0.bast-dev.ru',
-//     port = 1883,
-//     topic_prefix = "service/#weather_logger",
-//     username = 'hackathon',
-//     password = 'Autumn2021',
-// }
-//const client = mqtt.connect('mqtt://mqtt0.bast-dev.ru')
-
-export const obj = {
-  x: 1,
-  pol2: "xbit",
-  pol3: "mi stradaem",
-  pol4: 10000,
-  pol5: "dfpmh",
-};
+let bdata = client;
 
 app.use(express.static(path.resolve(__dirname, "script")));
 
--- serverdata.js
+++ serverdata.js
@@ -1,8 +1,51 @@
-const obj = {
-  x: 1,
-  pol2: "xbit",
-  pol3: "mi stradaem",
-  pol4: 10000,
-  pol5: "dfpmh",
-};
-export default obj;
+import mqtt from "mqtt";
+import Ably from "ably";
+
+const broker = "mqtt0.bast-dev.ru";
+const port = 1883;
+const topic_prefix = "service/weather_logger/#";
+const username = "hackathon";
+const password = "Autumn2021";
+
+var client = mqtt.connect("mqtt://mqtt0.bast-dev.ru:1883", {
+  username: "hackathon",
+  password: "Autumn2021",
+});
+var topic = "service/weather_logger/";
+console.log("1");
+client.on("connect", () => {
+  client.subscribe("service/weather_logger/#", (err) =>
+    err ? console.error : null
+  );
+  client.on("message", (topic, value) => {
+    console.log(topic, value.toString());
+  });
+});
+
+// client.on("connect", function () {
+//   console.log("client connected");
+//   client.subscribe(
+//     "service/weather_logger/outdoor_temperature",
+//     (err, granted) => {
+//       if (err) {
+//         console.error(err, "||", granted);
+//       }
+//       client.on("message", (topic, message) => {
+//         console.log(message.toString());
+//       });
+//       client.end();
+//     }
+//   );
+// });
+// client.on("message", function (topic_prefix, message) {
+//   // message is Buffer
+//   console.log(message.toString());
+//   client.end();
+// });
+// // export default obj = {
+//   x: 1,
+//   pol2: "xbit",
+//   pol3: "mi stradaem",
+//   pol4: 10000,
+//   pol5: "dfpmh",
+// };
